c4609c09d8bc53f55109c01d0c3455f76010f13b
Aleksandr Demshin <demshin@gmail.com>
2019-09-29 20:00:10 +0500

    add ssh keys to project metadata

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,9 +54,7 @@ resource "google_compute_firewall" "firewall_puma" {
 resource "google_compute_project_metadata" "user_key" {
   metadata = {
     ssh-keys = <<EOF
-appuser1:${file(var.public_key_path)}
-appuser2:${file(var.public_key_path)}
-appuser3:${file(var.public_key_path)}
+appuser1:${file(var.public_key_path)} appuser2:${file(var.public_key_path)} appuser3:${file(var.public_key_path)}
 EOF
   }
 }
