356243b45fcc513553288e234be7d28b05a2b6a3
Aleksandr Demshin <demshin@gmail.com>
2019-10-10 20:36:45 +0500

    add db_disk_image variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,6 +24,11 @@ variable app_disk_image {
   description = "Disk image for reddit app"
 }
 
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
+
 variable number_app_instances {
   description = "Number of app instances"
 }
