ed0da7fa8fd4898a41c302341bbc59d1ad23a0fa
Aleksandr Demshin <demshin@gmail.com>
2019-10-10 20:33:37 +0500

    terraform fmt

--- terraform/app.tf
+++ terraform/app.tf
@@ -18,20 +18,20 @@ resource "google_compute_instance" "app" {
   metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
-#   connection {
-#     type        = "ssh"
-#     host        = self.network_interface[0].access_config[0].nat_ip
-#     user        = "appuser"
-#     agent       = false
-#     private_key = file(var.private_key_path)
-#   }
-#   provisioner "file" {
-#     source      = "files/puma.service"
-#     destination = "/tmp/puma.service"
-#   }
-#   provisioner "remote-exec" {
-#     script = "files/deploy.sh"
-#   }
+  #   connection {
+  #     type        = "ssh"
+  #     host        = self.network_interface[0].access_config[0].nat_ip
+  #     user        = "appuser"
+  #     agent       = false
+  #     private_key = file(var.private_key_path)
+  #   }
+  #   provisioner "file" {
+  #     source      = "files/puma.service"
+  #     destination = "/tmp/puma.service"
+  #   }
+  #   provisioner "remote-exec" {
+  #     script = "files/deploy.sh"
+  #   }
 }
 
 resource "google_compute_address" "app_ip" {
