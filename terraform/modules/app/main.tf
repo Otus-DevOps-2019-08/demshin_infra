c559fcb715f4e8f6f8af48f0f3140c90e240e08e
Aleksandr Demshin <demshin@gmail.com>
2019-10-12 00:01:38 +0500

    add connection

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -18,13 +18,14 @@ resource "google_compute_instance" "app" {
   metadata = {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
-  #   connection {
-  #     type        = "ssh"
-  #     host        = self.network_interface[0].access_config[0].nat_ip
-  #     user        = "appuser"
-  #     agent       = false
-  #     private_key = file(var.private_key_path)
-  #   }
+  connection {
+    type        = "ssh"
+    host        = self.network_interface[0].access_config[0].nat_ip
+    user        = "appuser"
+    agent       = false
+    private_key = file(var.private_key_path)
+  }
+  
   provisioner "file" {
     source      = "${path.module}/files/puma.service"
     destination = "/tmp/puma.service"
