0a6bfaf805d1e441078d52b65c42cb59e179649e
Aleksandr Demshin <demshin@gmail.com>
2019-10-11 10:24:41 +0500

    separate vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,6 @@ provider "google" {
   region  = var.region
 }
 
-resource "google_compute_firewall" "firewall_ssh" {
-  name    = "default-allow-ssh"
-  network = "default"
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  description   = "Allow SSH from anywhere"
-  source_ranges = ["0.0.0.0/0"]
-}
-
 resource "google_compute_project_metadata" "user_key" {
   metadata = {
     ssh-keys = <<EOF
