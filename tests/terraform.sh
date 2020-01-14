33181e1754449a5f9f016f344c904af35d3d2bc0
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 19:59:00 +0500

    add terraform --version

--- tests/terraform.sh
+++ tests/terraform.sh
@@ -8,9 +8,10 @@ ROOT_DIR="$(pwd)"
 
 # install terraform
 
-wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip
-unzip terraform_0.12.19_linux_amd64.zip
-sudo mv -f terraform /usr/local/bin/
+# wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip
+# unzip terraform_0.12.19_linux_amd64.zip
+# sudo mv -f terraform /usr/local/bin/
+terraform --version
 
 # terraform validate
 
