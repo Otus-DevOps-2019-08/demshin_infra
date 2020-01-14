3eaec8940725dd51555c3a88a83f8fbcf2f515f5
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 13:48:19 +0500

    remove packer installing from validate script

--- packer/validate.sh
+++ packer/validate.sh
@@ -4,11 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-# install packer
-curl -SL https://releases.hashicorp.com/packer/1.5.1/packer_1.5.1_linux_amd64.zip -o packer_1.5.1_linux_amd64.zip
-unzip packer_1.5.1_linux_amd64.zip
-sudo mv packer /usr/bin/packer
-
 # validate packer templates
 packer validate -var-file=packer/variables-reddit-full.json.example packer/app.json
 packer validate -var-file=packer/variables-reddit-full.json.example packer/db.json
