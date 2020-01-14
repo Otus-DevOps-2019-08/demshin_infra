6970f22cda59648f3066d858056a02344d831a68
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 18:10:00 +0500

    fix bash syntax

--- tests/packer.sh
+++ tests/packer.sh
@@ -4,10 +4,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-$ROOT_DIR=pwd
+ROOT_DIR="$(pwd)"
 
 # validate packer templates
-cd $ROOT_DIR/packer
+cd "${ROOT_DIR}"/packer
 packer validate -var-file=variables-reddit-full.json.example immutable.json
 packer validate -var-file=variables-reddit-full.json.example ubuntu16.json
-cd $ROOT_DIR
+cd "${ROOT_DIR}"
--- tests/terraform.sh
+++ tests/terraform.sh
@@ -4,17 +4,17 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-$ROOT_DIR=pwd
+ROOT_DIR="$(pwd)"
 
 # terraform validate
 
-cd $ROOT_DIR/terraform
+cd "${ROOT_DIR}"/terraform
 terraform validate
-cd $ROOT_DIR/terraform/prod
+cd "${ROOT_DIR}"/terraform/prod
 terraform validate
-cd $ROOT_DIR/terraform/stage
+cd "${ROOT_DIR}"/terraform/stage
 terraform validate
 
 # tflint
 
-cd $ROOT_DIR/terraform
+cd "${ROOT_DIR}"/terraform
