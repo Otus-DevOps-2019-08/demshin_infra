35269f6b4eff3265dc4950f275957b15f462b290
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 14:17:49 +0500

    remove old packer files from validate

--- packer/validate.sh
+++ packer/validate.sh
@@ -5,8 +5,8 @@ set -o pipefail
 set -o nounset
 
 # validate packer templates
-packer validate -var-file=packer/variables-reddit-full.json.example packer/app.json
-packer validate -var-file=packer/variables-reddit-full.json.example packer/db.json
+# packer validate -var-file=packer/variables-reddit-full.json.example packer/app.json
+# packer validate -var-file=packer/variables-reddit-full.json.example packer/db.json
 cd packer
 packer validate -var-file=variables-reddit-full.json.example immutable.json
 packer validate -var-file=variables-reddit-full.json.example ubuntu16.json
