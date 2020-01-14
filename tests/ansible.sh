ac62302553b46c64099e187315d21170f3da1ee6
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 21:00:04 +0500

    add ansible check

--- tests/ansible.sh
+++ tests/ansible.sh
@@ -10,3 +10,9 @@ ROOT_DIR="$(pwd)"
 sudo pip install --upgrade pip
 sudo pip install ansible
 sudo pip install ansible-lint
+
+# Ansible Check
+echo "=============================="
+echo "Ansible Check"
+echo "=============================="
+ansible-lint -x 401 ${ROOT_DIR}/ansible/playbooks/*
