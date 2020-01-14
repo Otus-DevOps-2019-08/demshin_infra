7c00c2ac5d430e403fae069c823b15e06b18ae2c
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 18:15:58 +0500

    add install terraform

--- tests/terraform.sh
+++ tests/terraform.sh
@@ -6,6 +6,12 @@ set -o nounset
 
 ROOT_DIR="$(pwd)"
 
+# install terraform
+
+wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip
+unzip terraform_0.12.19_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
 # terraform validate
 
 cd "${ROOT_DIR}"/terraform
