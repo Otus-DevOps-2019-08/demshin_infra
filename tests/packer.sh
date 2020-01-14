f4f2938f720ad72a07f8b155145a33ea75cd9fef
Aleksandr Demshin <demshin@gmail.com>
2020-01-14 14:28:14 +0500

    move packer validate script

--- tests/packer.sh
+++ tests/packer.sh
@@ -5,6 +5,6 @@ set -o pipefail
 set -o nounset
 
 # validate packer templates
-cd packer
+cd ../packer
 packer validate -var-file=variables-reddit-full.json.example immutable.json
 packer validate -var-file=variables-reddit-full.json.example ubuntu16.json
